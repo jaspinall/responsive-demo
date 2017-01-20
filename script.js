d2a412b3c98bcffd3a6150852c3edb0b05d80af8
Julia Aspinall <juliaaspinall@Julias-MacBook-Air.local>
2017-01-19 19:51:27 -0800

    enabled mobile nav, added smaller background image

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const panels = document.querySelectorAll('.panel');
 const buttons = document.querySelector('.panelButton');
 const main = document.querySelector('.main');
 const mainHeight = getComputedStyle(main).height.split('px')[0];
-const nav = document.querySelector('.menu');
+const nav = document.querySelector('nav');
 const navHeight = getComputedStyle(nav).height.split('px')[0];
+const hamburger = document.querySelector('.hamburger');
+const closeMenu = document.querySelector('.closeMenu');
+const menu = document.querySelector('.menu');
 
+
+// gallery animation at the top
 function toggleOpen() {
   panels.forEach((panel) => {
     panel.classList.toggle('open', false);
@@ -22,15 +27,32 @@ function toggleOpen() {
 panels.forEach(panel => panel.addEventListener('mouseover', toggleOpen));
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
+
+// fix navbar at top for desktop displays
 function fixScroll(e) {
-  if( window.pageYOffset > mainHeight - navHeight) {
+  if(window.pageYOffset > mainHeight - navHeight) {
     nav.classList.remove('mobile');
     nav.classList.add('fixed');
   }
-  if( window.pageYOffset < mainHeight - navHeight) {
+  if(window.pageYOffset < mainHeight - navHeight) {
     nav.classList.remove('fixed');
     nav.classList.add('mobile');
   }
 }
 
-window.addEventListener('scroll', fixScroll, false);
+if (window.innerWidth > 450) {
+  window.addEventListener('scroll', fixScroll, false);
+}
+
+// open and close the menu on mobile
+hamburger.addEventListener('click', () => {
+  menu.style.transform = 'translateY(0)';
+  hamburger.style.visibility = 'hidden';
+  closeMenu.style.visibility = 'visible';
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.style.transform = 'translateY(-150%)';
+  hamburger.style.visibility = 'visible';
+  closeMenu.style.visibility = 'hidden';
+});
